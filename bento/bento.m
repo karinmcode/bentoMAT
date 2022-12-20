fb534cc5814e37e201daf00a6e5ffa5904aa3e89
karinmcode <karinmorandell.pro@gmail.com>
2022-12-20 16:35:03 -0500

    myslider
    changed slider code to object oriented version of it. No need for guidata anymore

--- bento/bento.m
+++ bento/bento.m
@@ -3,14 +3,15 @@
 % (C) Ann Kennedy, 2019
 % California Institute of Technology
 % Licensing: https://github.com/annkennedy/bento/blob/master/LICENSE.txt
-
+dbstop if error%KM provisory
 addpath(genpath(fileparts(mfilename('fullpath')))); %add all folders in the same directory as bento.m
+
 if(~exist('quitloop','var') || ~exist('gui','var') || ~isstruct(gui) || ~isfield(gui,'h0') || ~isvalid(gui.h0))
     fclose all;
     construct_gui();
 elseif(exist('gui','var') && isstruct(gui) && isfield(gui,'h0') && isvalid(gui.h0))
-    ans = questdlg('Keep data currently in Bento?','','Yes');
-    switch ans
+    ANS = questdlg('Keep data currently in Bento?','','Yes');
+    switch ANS
         case 'Yes'
             figure(gui.h0);
         case 'No'
@@ -23,23 +24,28 @@ end
 quitloop = 0;
 
 
-while(~quitloop)
-    gui = guidata(gui.h0);
-    if(any(gui.Action))
-        test = toc(gui.ctrl.slider.timer);
-        if(test>0.15)
-            if(~isempty(strfind(gui.Action,'drag')))
-                hSlider = setSliderVal(gui.h0);
-            else
-                hSlider = incSliderVal(gui.h0);
-            end
-            updateSlider(gui.h0,hSlider);
-            dummy.Source.Tag = 'slider';
-            updatePlot(gui.h0,dummy);
-        end
-    end
-    quitloop = gui.quitbutton;
-    drawnow;
-end
-set(gui.h0,'CloseRequestFcn','closereq');
-close(gui.h0);
+
+
+%gui.h0.KeyPressFcn
+%gui.h0.KeyReleaseFcn%edit evalKeyUp
+% 
+% while(~quitloop)
+%     gui = guidata(gui.h0);
+%     if(any(gui.Action))
+%         test = toc(gui.ctrl.slider.timer);
+%         if(test>0.15)
+%             if(~isempty(strfind(gui.Action,'drag')))
+%                 hSlider = setSliderVal(gui.h0);
+%             else
+%                 hSlider = incSliderVal(gui.h0);
+%             end
+%             updateSlider(gui.h0,hSlider);
+%             dummy.Source.Tag = 'slider';
+%             updatePlot(gui.h0,dummy);
+%         end
+%     end
+%     quitloop = gui.quitbutton;
+%     drawnow;
+% end
+% set(gui.h0,'CloseRequestFcn','closereq');
+% close(gui.h0);
---
+++ bento/startup/control bars/drawmyslider.m
@@ -0,0 +1,8 @@
+function slider = drawmyslider(gui,row,scale,nRows)
+%
+% (C) Karin Morandell, 2022
+% NYU
+
+
+slider=myslider(gui,row,scale,nRows);
+myaddeditfilemenu(slider.panel,'myslider');
