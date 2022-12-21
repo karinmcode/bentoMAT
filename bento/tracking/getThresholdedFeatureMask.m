fa8e64bd8d10eb96a9b0a1bb8d84b2e044c52b88
karinmcode <karinmorandell.pro@gmail.com>
2022-12-21 11:28:29 -0500

    fixed bug with upper and lower threshold limit for feature slider
    some variable renaming

--- bento/tracking/getThresholdedFeatureMask.m
+++ bento/tracking/getThresholdedFeatureMask.m
@@ -9,31 +9,47 @@ function [mask,params] = getThresholdedFeatureMask(gui,params)
 % create mask parameters from current plots
 if(~exist('params','var'))
     params = [];
-    for i=1:length(gui.features.feat) %iterate over all plotted features (assume we're ANDing them)
+    for ifeat=1:length(gui.features.feat) %iterate over all plotted features (assume we're ANDing them)
 
-        ch      = gui.features.feat(i).ch;
-        featNum = gui.features.feat(i).featNum;
+        ch      = gui.features.feat(ifeat).ch;
+        featNum = gui.features.feat(ifeat).featNum;
 
-        params(i).ch        = ch;
-        params(i).featNum   = featNum;
-        params(i).limL      = str2num(gui.features.feat(i).threshValL.String);
-        params(i).limU      = str2num(gui.features.feat(i).threshValU.String);
+        params(ifeat).ch        = ch;
+        params(ifeat).featNum   = featNum;
+        params(ifeat).limL      = str2num(gui.features.feat(ifeat).threshValL.String);
+        params(ifeat).limU      = str2num(gui.features.feat(ifeat).threshValU.String);
     end
 end
 
 if(isempty(params))
-    mask = false(1,size(gui.data.tracking.features{1},1));
+    mask = false(1,numel(gui.data.tracking.features{1}));
     return
 end
 
 % apply parameters to tracking data
-mask = true(1,size(gui.data.tracking.features{1},1));
-for i=1:length(params)
-    vals        = gui.data.tracking.features{params(i).ch}(:,params(i).featNum);
-    if(params(i).limL>params(i).limU)
-        mask = mask & ((vals<=params(i).limL) & (vals>=params(i).limU))';
+NFrames = length(gui.data.annoTime);
+vals        = gui.data.tracking.features{:,params(ifeat).featNum};%(:,params(i).featNum);
+
+mask = true(1,numel(vals));%KM fix
+for ifeat=1:length(params)
+    %vals        = gui.data.tracking.features{params(i).ch}(:,params(i).featNum);
+    vals        = gui.data.tracking.features{:,params(ifeat).featNum};%(:,params(i).featNum);
+    vals = vals(:)';
+    limL = params(ifeat).limL;
+    limU = params(ifeat).limU;
+    if(limL<limU)
+        mask = mask & (  (vals>=limL) & (vals<=limU)  );
     else
-        mask = mask & ((vals<=params(i).limL) | (vals>=params(i).limU))';
+        mask = mask & (  (vals<=limL) | (vals>=limU)  );
     end
 end
-mask = convertToRast(floor(gui.data.trackTime(convertToBouts(mask))*gui.data.annoFR),length(gui.data.annoTime));
+
+
+
+% corrrect for different frame rates?
+% not sure why this is here? maybe for case of annotations being different fs from features
+Bouts = convertToBouts(mask);
+mask = convertToRast(floor(gui.data.trackTime(Bouts)*gui.data.annoFR),NFrames);
+
+
+
