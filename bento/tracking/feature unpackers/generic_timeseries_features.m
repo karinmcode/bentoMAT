ceb1f8743f93412c13644f67a0a56d213875d199
karinmcode <karinmorandell.pro@gmail.com>
2022-12-21 10:26:33 -0500

    correction for several variables import with different length
    - added comments as well
    - not sure that works for other user's data though

--- bento/tracking/feature unpackers/generic_timeseries_features.m
+++ bento/tracking/feature unpackers/generic_timeseries_features.m
@@ -1,19 +1,43 @@
 function [features, names] = generic_timeseries_features(args)
-
+% features : rows are number of features and columns time/samples
 f = fieldnames(args);
 if length(f)>1
+% 	disp('not sure which variables to unpack, features will not be loaded.');
+%     questdlg()
+% 	features = [];
+% 	names = '';
+% 	return
+end
+num_feats = numel(f);
+num_sample_xfeat = nan(num_feats,1);
+for ifi = 1:num_feats
+    fi = f{ifi};
+    num_sample_xfeat(ifi) = numel(args.(fi));
+    sz = size(args.(fi));
+end
+nsamp = num_sample_xfeat(1);
+if ~all(num_sample_xfeat==nsamp)
 	disp('not sure which variables to unpack, features will not be loaded.');
+    disp(num_sample_xfeat)
+
+    ANS=questdlg('Fix data to make them all same length?');
+    if ANS(1)=='Y'
+        n = min(num_sample_xfeat);
+        for ifi = 1:num_feats
+            fi = f{ifi};
+            args.(fi) = args.(fi)(1:n);
+        end
+    else
 	features = [];
 	names = '';
-	return
+	return;
+    end
 end
 
-num_feats = min(size(args.(f{:})));
-
-if size(args.(f{:}),2)==num_feats
-	features = permute(double(args.(f{:})),[3,1,2]);
-else
-	features = permute(double(args.(f{:})'),[3,1,2]);
+features = nan(num_feats,nsamp);
+for ifi = 1:num_feats
+    fi = f{ifi};
+    features(ifi,:) = args.(fi);
 end
 
-names = strcat([f{:} '-'], num2str((1:num_feats)'));
+names = f;
