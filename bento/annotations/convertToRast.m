0884d3dee348b53a4eea7a989f7e71c1dd95bea8
karinmcode <karinmorandell.pro@gmail.com>
2022-12-21 09:14:37 -0500

    enhanced clarity
    added comments, renamed some variables

--- bento/annotations/convertToRast.m
+++ bento/annotations/convertToRast.m
@@ -1,14 +1,15 @@
-function rast = convertToRast(bhvr,tmax)
-%
+function rast = convertToRast(bhvr,max_index)
+%% rast = convertToRast(bhvr,max_index)
 % (C) Ann Kennedy, 2019
 % California Institute of Technology
 % Licensing: https://github.com/annkennedy/bento/blob/master/LICENSE.txt
 
 
 
-time = 1:tmax;
-rast = false(size(time));
-for i = 1:size(bhvr,1)
-    inds = (time>=bhvr(i,1))&(time<=bhvr(i,2))&(time<=tmax);
+indexes = 1:max_index;
+rast = false(size(indexes));
+nrows = size(bhvr,1);
+for i = 1:nrows
+    inds = (indexes>=bhvr(i,1))&(indexes<=bhvr(i,2))&(indexes<=max_index);
     rast(inds)=true;
 end
--- bento/annotations/copyChannel.m
+++ bento/annotations/copyChannel.m
@@ -13,17 +13,17 @@ end
 newCh               = strrep(newStr,' ','_');
 sourceCh            = strrep(sourceStr,' ','_');
 
-%save the new channel name:
+%% save the new channel name:
 channels            = gui.annot.channels;
 channels{end+1}     = newCh;
 channels            = unique(channels); %remove duplicate channel names
 gui.annot.channels  = channels;
 
-% copy the channel in gui.data
+%% copy the channel in gui.data
 gui.data.annot.(newCh) = gui.data.annot.(sourceCh);
 gui.data.annot = orderfields(gui.data.annot);
 
-% copy the channel in gui.allData
+%% copy the channel in gui.allData
 sessionList = fieldnames(gui.allData);
 mask = false(1,length(gui.allData));
 for i=1:length(sessionList)
