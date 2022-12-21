f0c1fcaef4446e5caf5775fc770dd56ff1a50cfc
karinmcode <karinmorandell.pro@gmail.com>
2022-12-21 10:19:56 -0500

    just commenting and added click right edit menus

--- bento/startup/drawFeatures.m
+++ bento/startup/drawFeatures.m
@@ -17,31 +17,44 @@ function gui = drawFeatures(gui)
     features.axes.XLim    = [-20 20];
     features.feat         = [];
     features.featsByChannel = {};
-
+%% features.channel
     features.channels     = uicontrol('parent',features.panel,'Style','popupmenu',...
                             'String',{''},'units','normalized','Position', [.05 0.015 .125 0.05],...
                             'Callback',@setFeatMenu);
+        myaddeditfilemenu(features.channels,{'drawFeatures' 'setFeatMenu'});
+%% features.menu 
     features.menu         = uicontrol('parent',features.panel,'Style','popupmenu',...
                             'String',{''},'units','normalized','Position', [.175 0.015 .34 0.05]);
+    %% add feature
+
     uicontrol('parent',features.panel,'Style','pushbutton','String','Add',...
                             'units','normalized','Position', [.525 0.01 .075 .08],...
                             'Callback',@addFeature);
-    uicontrol('parent',features.panel,'Style','pushbutton','String','<html>Add<br/>All</html>',...
+
+    %% add feature
+   h= uicontrol('parent',features.panel,'Style','pushbutton','String','<html>Add<br/>All</html>',...
                             'units','normalized','Position', [.6 0.01 .075 .08],...
                             'Callback',@addFeature);
+    myaddeditfilemenu(h,{'drawFeatures' 'addFeature'});
+
 
     % for thresholding features and making new annotations
     features.threshOn = uicontrol('parent',features.panel,'Style','pushbutton','String','Edit Thresholds',...
                             'units','normalized','Position', [.675 0.01 .25 .08],...
                             'Callback',{@toggleFeatThresholds,1});
+    myaddeditfilemenu(features.threshOn,{'drawFeatures' 'toggleFeatThresholds'});
 
-    % hidden buttons that are shown while we're working with feature thresholds
+    %% hidden buttons that are shown while we're working with feature thresholds
+    %% Cancel
     features.threshOff = uicontrol('parent',features.panel,'Style','pushbutton','String','Cancel',...
                             'units','normalized','Position', [.8 0.01 .15 .08],...
                             'Callback',{@toggleFeatThresholds,0},'visible','off');
+    myaddeditfilemenu(features.threshOff,{'drawFeatures' 'toggleFeatThresholds'});
+    %% Save button
     features.threshSave = uicontrol('parent',features.panel,'Style','pushbutton','String','Save',...
                             'units','normalized','Position', [.645 0.01 .15 .08],...
                             'Callback',@saveFeatThresholds,'visible','off');
+    myaddeditfilemenu(features.threshOff,{'drawFeatures' 'saveFeatThresholds'});
 
     gui.features = features;
 end
