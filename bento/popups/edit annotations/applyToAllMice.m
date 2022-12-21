f7cd4f4fd7710bc90bca58ec03a927f6552a0e23
karinmcode <karinmorandell.pro@gmail.com>
2022-12-21 11:39:39 -0500

    added update legend and gui.annot.bhv after merge

--- bento/popups/edit annotations/applyToAllMice.m
+++ bento/popups/edit annotations/applyToAllMice.m
@@ -6,7 +6,7 @@ function gui = applyToAllMice(gui,action,varargin)
 
 
 
-data    = gui.allData;
+allData    = gui.allData;
 inds    = gui.allPopulated;
 
 switch action
@@ -27,7 +27,7 @@ for i = 1:size(inds,1)
     m       = inds(i,1);
     sess    = ['session' num2str(inds(i,2))];
     trial   = inds(i,3);
-    anno    = data(m).(sess)(trial).annot;
+    anno    = allData(m).(sess)(trial).annot;
     
     channels = fieldnames(anno);
     for ch = 1:length(channels)
@@ -41,6 +41,8 @@ for i = 1:size(inds,1)
                 end
                 
             case 'merge'
+
+
                 if(~isfield(anno.(channels{ch}),newName))
                     anno.(channels{ch}).(newName) = [];
                 end
@@ -53,16 +55,26 @@ for i = 1:size(inds,1)
                 end
                 newRast = cleanMergedRaster(newRast);
                 anno.(channels{ch}).(newName) = newRast;
-                
+                gui.data.annot = anno;%KM
+                if(toKill && b>1)
+                    oldList2 = intersect(oldList,fieldnames(gui.annot.bhv));%KM
+                    gui.annot.bhv = rmfield( gui.annot.bhv,oldList2);
+                    
+                end
+                % update gui.annot.bhv
+                gui=my_update_gui_annot_bhv(gui);
+
+                updateLegend(gui,1)
             case 'rename'
                 anno.(channels{ch}).(newName) = anno.(channels{ch}).(oldName);
                 anno.(channels{ch}) = rmfield(anno.(channels{ch}),oldName);
         end
     end
-    data(m).(sess)(trial).annot = anno;
+    allData(m).(sess)(trial).annot = anno;
 end
 
-gui.allData = data;
+gui.allData = allData;
+
 
 
 
