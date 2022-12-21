ed1bc4984f2e1778c11c8ebb9f403b2c8c6d7e49
karinmcode <karinmorandell.pro@gmail.com>
2022-12-21 09:21:46 -0500

    enhanced clarity
    Just added comment for help to display the inputs of function

--- bento/annotations/updateLegend.m
+++ bento/annotations/updateLegend.m
@@ -1,5 +1,5 @@
 function updateLegend(gui,showHidden)
-%
+% updateLegend(gui,showHidden)
 % (C) Ann Kennedy, 2019
 % California Institute of Technology
 % Licensing: https://github.com/annkennedy/bento/blob/master/LICENSE.txt
